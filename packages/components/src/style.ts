288d6a538c292b994a9acd36fd2397c457339df0
zfitness <hunkzheng@tencent.com>
2022-06-28 19:20:42 +0800

    feat: 去掉无用的样式

--- packages/components/src/style.ts
+++ packages/components/src/style.ts
@@ -1,2 +1,6 @@
+// auto generated code
+import './form-button-group/style.less'
 import './form-item/style.less'
-
+import './form-layout/style.less'
+import './preview-text/style.less'
+import './radio/style.less'
